39bf5fbeb0ff8b32eafc2118e9b7b810b585fc7d
Miran <Emilbekov.m091@mail.ru>
2025-04-25 00:38:54 +0600

    Реализовать Header

--- src/Blog.js
+++ src/Blog.js
@@ -1,32 +1,48 @@
 import { Route, Routes } from "react-router-dom";
+import { Header } from "./components";
 import styled from "styled-components";
 
 const Content = styled.div`
-  padding: 120px 0;
+	padding: 120px 0;
 `;
+
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background-color: #fff;
+`;
+
 const H2 = styled.h2`
-  text-align: center;
+	text-align: center;
 `;
-const Header = () => <div>Header</div>;
-const Footer = () => <div>FOOTER</div>;
+
+// const Footer = () => <div>FOOTER</div>;
+
+// const Footer = styled(Footer)`
+// 	position: fixed;
+// `;
 
 export const Blog = () => {
-  return (
-    <>
-      <Header />
-      <Content>
-        <H2>CONTENT</H2>
-        <Routes>
-          <Route path="/" element={<div>Главная страница</div>} />
-          <Route path="/login" element={<div>Авторизация</div>} />
-          <Route path="/register" element={<div>Регистрация</div>} />
-          <Route path="/users" element={<div>Пользователи</div>} />
-          <Route path="/post" element={<div>Новая Статья</div>} />
-          <Route path="/post/:postId" element={<div>Статья</div>} />
-          <Route path="*" element={<div>ОШИБКА</div>} />
-        </Routes>
-      </Content>
-      <Footer />
-    </>
-  );
+	return (
+		<AppColumn>
+			<Header />
+			<Content>
+				<H2>CONTENT</H2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/login" element={<div>Авторизация</div>} />
+					<Route path="/register" element={<div>Регистрация</div>} />
+					<Route path="/users" element={<div>Пользователи</div>} />
+					<Route path="/post" element={<div>Новая Статья</div>} />
+					<Route path="/post/:postId" element={<div>Статья</div>} />
+					<Route path="*" element={<div>ОШИБКА</div>} />
+				</Routes>
+			</Content>
+			{/* <Footer /> */}
+		</AppColumn>
+	);
 };
